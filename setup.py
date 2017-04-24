5b3a52e68584ca302d5bf5751b9ba69c660c3f12
Ryan Gavin <rgavin@mediamorph.com>
2017-04-24 18:03:35 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='voteit',
+      version='1.0',
+      description='Upvotes a target submission using configured accounts and proxies',
+      url='http://github.com/rgavin/voteit',
+      author='Ryan Gavin',
+      author_email='ryan.michael.gavin@gmail.com',
+      license='MIT',
+      packages=['voteit'],
+      zip_safe=False)
